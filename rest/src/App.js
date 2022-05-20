9ac89c32f86fa58fb3b81ccae35be80c9bb3a569
RoshDumbb <roshanidumbhare30@gmail.com>
2022-05-20 23:36:57 +0530

    created json data

--- rest/src/App.js
+++ rest/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
 import Google from './components/Google';
-
+import MultipleSelect from './components/Restaurant';
 function App() {
   return (
     <div className="App">
+      <MultipleSelect/>
      <Google/>
     </div>
   );
