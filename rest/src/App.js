01873e8f4f8482a199052a01f0cd8c2fcf4fc6f4
RoshDumbb <roshanidumbhare30@gmail.com>
2022-05-20 22:58:30 +0530

    render map successfully

---
+++ rest/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Google from './components/Google';
+
+function App() {
+  return (
+    <div className="App">
+     <Google/>
+    </div>
+  );
+}
+
+export default App;
